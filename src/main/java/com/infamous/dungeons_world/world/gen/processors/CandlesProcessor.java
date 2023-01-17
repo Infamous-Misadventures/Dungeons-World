e6ed8920d9f7e4fb869276a2af768d1d01eca905
Patrigan <patrick.provinciael@gmail.com>
2023-01-18 00:23:25 +0100

    Updates to the crypt

--- src/main/java/com/infamous/dungeons_world/world/gen/processors/CandlesProcessor.java
+++ src/main/java/com/infamous/dungeons_world/world/gen/processors/CandlesProcessor.java
@@ -4,13 +4,19 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import mod.patrigan.structure_toolkit.util.RandomType;
 import mod.patrigan.structure_toolkit.world.gen.processors.AttachmentProcessor;
+import mod.patrigan.structure_toolkit.world.gen.processors.ProcessorUtil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.CandleBlock;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessor;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorType;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.Random;
 
 import static com.infamous.dungeons_world.world.gen.processors.ModProcessors.CANDLES;
 import static mod.patrigan.structure_toolkit.util.RandomType.RANDOM_TYPE_CODEC;
@@ -37,7 +43,12 @@ public class CandlesProcessor extends StructureProcessor {
     @Override
     public StructureTemplate.StructureBlockInfo process(LevelReader world, BlockPos piecePos, BlockPos structurePos, StructureTemplate.StructureBlockInfo rawBlockInfo, StructureTemplate.StructureBlockInfo blockInfo, StructurePlaceSettings settings, StructureTemplate template) {
         AttachmentProcessor attachmentProcessor = new AttachmentProcessor(vanilla, 2, false, true, rarity, randomType);
-        return attachmentProcessor.process(world, piecePos, structurePos, rawBlockInfo, blockInfo, settings, template);
+        StructureTemplate.StructureBlockInfo processed = attachmentProcessor.process(world, piecePos, structurePos, rawBlockInfo, blockInfo, settings, template);
+        if(ForgeRegistries.BLOCKS.getKey(processed.state.getBlock()).equals(vanilla)){
+            RandomSource random = ProcessorUtil.getRandom(randomType, blockInfo.pos, piecePos, structurePos, world, SEED);
+            return new StructureTemplate.StructureBlockInfo(processed.pos, processed.state.setValue(CandleBlock.CANDLES, random.nextInt(3)+1).setValue(CandleBlock.LIT, true), processed.nbt);
+        }
+        return blockInfo;
     }
 
     protected StructureProcessorType<?> getType() {
